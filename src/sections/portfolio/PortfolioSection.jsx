02a35eb7c2edaa0cb88cb41e935753c8ae8c2a5c
Laynara S Vieira <contatolaynarasvieira@gmail.com>
2026-06-22 16:02:05 -0300

    enviando arquivos para dev

--- src/sections/portfolio/PortfolioSection.jsx
+++ src/sections/portfolio/PortfolioSection.jsx
@@ -22,8 +22,8 @@ export default function PortfolioSection() {
 
   const handleCloseModal = () => {
     setSelectedItem(null);
-  };
-
+  }
+  
   return (
     <section id="projects" className="section">
       <Container>
